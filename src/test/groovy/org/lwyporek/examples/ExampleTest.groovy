96b8d8a4c241a1625432fbbb8d1612d467e83b46
Lukasz Wyporek <lwyporek@akamai.com>
2017-12-06 13:17:14 +0100

    explicitly setting path to chromedriver

--- src/test/groovy/org/lwyporek/examples/ExampleTest.groovy
+++ src/test/groovy/org/lwyporek/examples/ExampleTest.groovy
@@ -4,6 +4,10 @@ import org.fluentlenium.adapter.spock.FluentSpecification
 
 class ExampleTest extends FluentSpecification {
 
+    def setupSpec() {
+        System.setProperty("webdriver.chrome.driver", "/Users/lwyporek/workspace/chromedriver")
+    }
+
     def "Should have expected title"() {
         when:
             goTo("http://www.example.com")
